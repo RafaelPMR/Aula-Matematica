ab8a7b05f65f7293f5868b2f94d4457a0915508b
Rafael melo <rafael.pmr@hotmail.com>
2026-03-26 09:49:15 -0300

    Final

--- pg definitivo.py
+++ pg definitivo.py
@@ -1,15 +1,14 @@
 class ProgressaoGeo:
-    def __init__(self, primeiroTermo, razao):
-        self.primeiroTermo = primeiroTermo
-        self.razao = razao
+    def __init__(self, primeiroTermo, razao): #função construtora que define o primeiro termo e a razão
+        self.primeiroTermo = primeiroTermo  #porque usa self? self é uma referência à instância da classe com isso a função consegue acessar os atributos da classe
+        self.razao = razao 
+    def calcular_termo(self, n): #função calcular_termo que calcula o n-ésimo termo
+        if self.razao == 0: raise ValueError("Razão não pode ser zero") 
+        return self.primeiroTermo * (self.razao ** (n - 1)) #retorna o n-ésimo termo
 
-    def calcular_termo(self, n):
-        if self.razao == 0: raise ValueError("Razão não pode ser zero")
-        return self.primeiroTermo * (self.razao ** (n - 1))
-
-    def calcular_soma(self, n):
+    def calcular_soma(self, n): 
         if self.razao == 1:
-            return self.primeiroTermo * n
+            return self.primeiroTermo * n 
         return self.primeiroTermo * (self.razao ** n - 1) / (self.razao - 1)
 
     def exibir(self, n):
@@ -17,5 +16,5 @@ class ProgressaoGeo:
             print(self.calcular_termo(i))
 
 pg = ProgressaoGeo(2,3)
-pg.exibir(5)
+pg.exibir(1)
 
